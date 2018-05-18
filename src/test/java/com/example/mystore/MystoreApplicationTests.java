3fafb0e06f2ac7b1106f6314abc3d93bb9663f44
thomasdac <thomasdac@gmail.com>
2018-05-18 01:54:20 -0300

    limpeza do application properties (dados de conexão carregados através de variáveis de ambiente) e inclusão do actuator

--- src/test/java/com/example/mystore/MystoreApplicationTests.java
+++ src/test/java/com/example/mystore/MystoreApplicationTests.java
@@ -2,8 +2,6 @@ package com.example.mystore;
 
 import com.example.mystore.entity.Product;
 import com.example.mystore.repo.ProductRepo;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
